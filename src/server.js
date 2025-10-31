700ad6ae7f918c9b8220476c6a589754c5e076a0
idrisregg <idrisregg@gmail.com>
2025-11-01 00:07:41 +0100

    Added env for CORS Restriction

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const fastify = Fastify({
 });
 
 fastify.register(cors, {
-    origin: ['http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL||'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 });
