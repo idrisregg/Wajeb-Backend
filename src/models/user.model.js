44311be2734026ddeebebdeab6dcd77c7b08dfd3
idrisregg <idrisregg@gmail.com>
2025-11-05 19:12:47 +0100

    Replaced bcrypt with Argon2 for best password encryption

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,39 +1,48 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import * as argon2 from "argon2";
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minLength: 5
-    },
-    userName: {
-        type: String,
-        unique: true,
-        required: true,
-        minLength: 5
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 6
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 5,
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 5,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 6,
+  },
 });
 
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    this.password = await argon2.hash(this.password);
     next();
+  } catch (err) {
+    next(err);
+  }
 });
 
-UserSchema.methods.comparePassword = async function(enteredPassword) {
-    return bcrypt.compare(enteredPassword, this.password);
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  try {
+    return await argon2.verify(this.password, enteredPassword);
+  } catch (err) {
+    return false;
+  }
 };
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
